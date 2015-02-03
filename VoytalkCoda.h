6df43d7f709946f1825562d46d36a11d223a590e
Marcus Chang <marcus.chang@arm.com>
2015-02-03 13:35:31 +0000

    Added VoytalkCoda constructor. Need mutators.

---
+++ VoytalkCoda.h
@@ -0,0 +1,22 @@
+#ifndef __VOYTALKCODA_H__
+#define __VOYTALKCODA_H__
+
+#include "CborMap.h"
+#include "CborString.h"
+
+enum {
+    VOYTALK_CODA = 0x400F
+};
+
+class VoytalkCoda : public CborMap
+{
+public:
+    VoytalkCoda(uint32_t id, uint32_t success) : CborMap(2)
+    {
+        CborMap::setTag(VOYTALK_CODA);
+        CborMap::insert("invocation", id);
+        CborMap::insert("success", success);
+    }
+};
+
+#endif // __VOYTALKCODA_H__
